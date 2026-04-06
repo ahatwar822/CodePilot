200c0501ae3ffdba987ce28972748adaf22f7abc
ahatwar822 <himanshugaupale875@gmail.com>
2026-04-07 00:54:27 +0530

    Connected MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import app from './src/app.js';
 import dotenv from 'dotenv'
+import { connectDb } from './src/config/db.js';
 
 dotenv.config({ quiet: true});
 
 const PORT = process.env.PORT || 5000;
+
+connectDb();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
