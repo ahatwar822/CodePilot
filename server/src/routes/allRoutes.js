7247321bc996b1d4312d57c8ea794224623266c8
ahatwar822 <himanshugaupale875@gmail.com>
2026-04-16 01:33:19 +0530

    refactor(auth): protect routes using isAuthenticated middleware

--- server/src/routes/allRoutes.js
+++ server/src/routes/allRoutes.js
@@ -5,14 +5,15 @@ import fileRouter from './files/fileRoutes.js';
 import folderRouter from './folders/folderRoutes.js';
 import aiRouter from './Ai/ai.routes.js';
 import authRouter from './Auth/auth.routes.js';
+import isAuthenticated from '../middlewares/isAuthenticated.js';
 
 const router = express.Router();
 
 router.use('/user',userRouter);
 router.use('/admin', adminRouter);
-router.use('/file', fileRouter)
-router.use('/folder', folderRouter);
-router.use('/ai', aiRouter);
+router.use('/file',isAuthenticated, fileRouter)
+router.use('/folder',isAuthenticated, folderRouter);
+router.use('/ai',isAuthenticated, aiRouter);
 router.use('/auth', authRouter)
 
 
