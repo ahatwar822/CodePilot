b2876763bc465111a0ddfa73a15616cb1777d0e3
ahatwar822 <himanshugaupale875@gmail.com>
2026-04-16 03:32:24 +0530

    feat: add folder and file model for code storage

---
+++ server/src/models/file.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+  language: {
+    type: String,
+    default: "javascript",
+  },
+  folder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "folders",
+    default: null,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+}, { timestamps: true });
+
+const File = mongoose.model("files", fileSchema);
+
+export default File;
